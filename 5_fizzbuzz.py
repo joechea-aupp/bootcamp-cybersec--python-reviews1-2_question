35e6d6e6d193b5d563deefabaaa9aef17eda6a6a
Singhtararaksmey (Joe) Chea <78744570+joechea-aupp@users.noreply.github.com>
2024-05-16 09:50:51 +0700

    Update 5_fizzbuzz.py

--- 5_fizzbuzz.py
+++ 5_fizzbuzz.py
@@ -2,4 +2,5 @@
 # Write a program that prints the numbers from 1 to 25. For number that divisible 
 # by three print “Fizz” instead of the number and if it is divisible by of five 
 # print “Buzz”. For numbers that is divisible by both three and five print “FizzBuzz”.
+# if none of the criteria met, print the number.
 
